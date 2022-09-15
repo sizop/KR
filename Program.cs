4e68765f7f1fb3fa3baa54127be8810f51b4fa2e
Dmirty <9048881951@mail.ru>
2022-09-15 19:46:09 +0500

    15/09 #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Максимальное количество символов в элементе: ");
+int symbol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Количество элементов массива: ");
+int element = Convert.ToInt32(Console.ReadLine());
+string [] GetArray (int m, int n) // метод ввода массива 
+{
+	string[] Array = new string[n];
+	int stringlen = new Random().Next(1, m), randValue;
+	char letter;
+	for (int k = 0; k < n; k++)
+	{
+		string str = ""; stringlen = new Random().Next(1, m);
+		for (int i = 0; i < stringlen + 1; i++)
+		{
+			randValue = new Random().Next(26);
+			letter = Convert.ToChar(randValue + 65);
+			str = str + letter;
+		}
+		Array[k] = str;
+	}
+	return Array;
+}
+string[] matrix = GetArray(symbol, element);
+for (int j = 0; j < matrix.GetLength(0); j++) //вывод массива
+{ Console.Write(matrix[j] + "\t"); }
